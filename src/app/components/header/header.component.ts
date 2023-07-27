1c38ae0d3137e38742ac25b5c91921da844127ea
Leandro Cabeza <cabezaleandro@gmail.com>
2023-07-27 18:09:14 -0300

    Added sessions and logount and login actions

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.interface';
+import { UsuariosService } from 'src/app/service/usuarios.service';
+import swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  usuarioLogueado: Usuario | null = null;
+  userIcon =  "";
 
-  constructor() {
-   }
+  constructor(private usuariosService: UsuariosService) {}
 
   ngOnInit(): void {
+    this.usuariosService.usuarioLogueado$.subscribe((usuario) => {
+      this.usuarioLogueado = usuario;
+      this.userIcon = this.usuarioLogueado?.tipoUsuario === 'especialista' ? '🩺' : this.usuarioLogueado?.tipoUsuario === 'admin' ? '🔨' : '🟢';
+    });
   }
+
+  logout() {
+    this.usuariosService.logout();
+    swal.fire('Desloggeo exitoso!', 'Te desloggeaste correctemante, te esperamos pronto ❤', 'success');
+  }
+
 }
--- src/app/service/usuarios.service.ts
+++ src/app/service/usuarios.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Usuario } from '../models/usuario.interface';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
@@ -14,12 +14,21 @@ export class UsuariosService {
 
   usuarioColleccion!: AngularFirestoreCollection<Usuario>;
   usuarios!: Observable<Usuario[]>;
+  private usuarioLogueadoSubject: BehaviorSubject<Usuario | null> = new BehaviorSubject<Usuario | null>(null);
+  usuarioLogueado$: Observable<Usuario | null> = this.usuarioLogueadoSubject.asObservable(); // Expone el Observable para suscribirse
+  private tiempoExpiracionSesion: number = 30 * 60 * 1000; // 30 minutos en milisegundos
+  private usuarioLocalStorageKey = 'usuarioLogueado'; // Clave para guardar el usuario en el LocalStorage
 
   constructor( 
     private db: AngularFirestore,
     private storage: AngularFireStorage) {
-    this.usuarioColleccion = this.db.collection<Usuario>('usuarios'); // Inicializa la colección aquíOpción adicional para inicializar la lista de usuarios
-  }
+      this.usuarioColleccion = this.db.collection<Usuario>('usuarios');
+      const usuarioGuardado = localStorage.getItem(this.usuarioLocalStorageKey);
+      if (usuarioGuardado) {
+        this.usuarioLogueadoSubject.next(JSON.parse(usuarioGuardado));
+        this.setTiempoExpiracion();
+      }
+    }
 
   async uploadImage(image: File): Promise<string> {
     const storageRef = this.storage.ref(`user-profile-images/${image.name}`);
@@ -58,15 +67,35 @@ export class UsuariosService {
         .toPromise()
         .then((querySnapshot) => {
           if (querySnapshot?.empty) {
-            resolve(null); 
+            resolve(null);
           } else {
             querySnapshot?.forEach((doc) => {
-              resolve(doc.data() as Usuario);
+              const usuario = doc.data() as Usuario;
+              this.usuarioLogueadoSubject.next(usuario); // Emitir el usuario logueado a través del BehaviorSubject
+              localStorage.setItem(this.usuarioLocalStorageKey, JSON.stringify(usuario)); // Guardar usuario en el LocalStorage
+              this.setTiempoExpiracion(); // Establecer el tiempo de expiración para la sesión
+              resolve(usuario);
             });
           }
         })
         .catch((error) => reject(error));
     });
+  }
+
+  private setTiempoExpiracion(): void {
+    setTimeout(() => {
+      this.usuarioLogueadoSubject.next(null); // Usuario expirado, emite null a través del BehaviorSubject
+      localStorage.removeItem(this.usuarioLocalStorageKey); // Eliminar usuario del LocalStorage al expirar
+    }, this.tiempoExpiracionSesion);
+  }
+
+  getUsuarioLogueado(): Usuario | null {
+    return this.usuarioLogueadoSubject.getValue();
+  }
+
+  logout(): void {
+    this.usuarioLogueadoSubject.next(null); 
+    localStorage.removeItem(this.usuarioLocalStorageKey);
   }
 
 }
