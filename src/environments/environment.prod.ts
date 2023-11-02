aee6616b012e9e380adcfcbcbe797b25927d7566
Leandro Cabeza <cabezaleandro@gmail.com>
2023-11-02 17:57:44 -0300

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDi6KQyoX5fKzAGNVZRhJy1d8mP7Ft6COo",
+    authDomain: "clinica-696af.firebaseapp.com",
+    projectId: "clinica-696af",
+    storageBucket: "clinica-696af.appspot.com",
+    messagingSenderId: "470638544925",
+    appId: "1:470638544925:web:26130ed9ba6683d6abdef4"
+  }
 };
