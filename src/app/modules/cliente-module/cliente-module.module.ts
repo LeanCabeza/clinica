476c911c6f488bef695f7fe4ea100a9d5091ebcb
Leandro Cabeza <cabezaleandro@gmail.com>
2023-11-20 20:49:50 -0300

    Created modules

---
+++ src/app/modules/cliente-module/cliente-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClienteModuleRoutingModule } from './cliente-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClienteModuleRoutingModule
+  ]
+})
+export class ClienteModuleModule { }
